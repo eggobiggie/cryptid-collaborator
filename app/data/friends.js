3cd6ceb74ff3b35d17f17790c5137cd329446bb4
eggobiggie <miacrandell@gmail.com>
2018-02-12 15:31:12 -0700

    add friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,191 @@
 var cryptids = [
     {
         name: "Sasquatch",
-        photo: 
+        photo: "https://sasquatchchronicles.com/wp-content/uploads/2016/12/Barmanou-302x300.jpg",
+        description: "As a lover of the forest, this guy is a good one to have on your side for maximum spooking in the great outdoors. This cryptid tends to have an unpleasant body odor, so be sure to pack some deodorizer, and of course offer it nicely, otherwise he might not want to work with you. Our friend Sasquatch is sometimes a fan of leaving large footprints behind, which is an ideal scenario for sparking your terrified friends' imaginations into a frightened frenzy!",
+        scores: [
+            3,
+            1,
+            2,
+            5,
+            1,
+            5,
+            2,
+            1,
+            2,
+            1
+        ]
+    },
+    {
+        name: "The Loch Ness Monster",
+        photo: "https://www.thesun.co.uk/wp-content/uploads/2016/10/nintchdbpict0002368798841.jpg?strip=all&w=960",
+        description: "If you're looking for a destination spooking, look no further than the Scottish Highlands, where you'll find the Loch Ness Monster. A slippery sort of fellow, this cryptid enjoys creating an ambiance of mystery and intrigue, only showing himself on occasion, and making sure to be seen only out of the corner of the eye, so no one can be truly sure about what they saw. With the Nessie's help, your friends will never want to swim again!",
+        scores: [
+            1,
+            2,
+            2,
+            4,
+            4,
+            1,
+            3,
+            2,
+            3,
+            1
+        ]
+    },
+    {
+        name: "Hellhound",
+        photo: "https://ferrebeekeeper.files.wordpress.com/2014/09/dscf6705-700x478.jpg",
+        description: "Nothing says man's best friend like a hellhound. With glowing red eyes and a howl that can only mean death, this mutt will surely bring shivers down your friends' spines. Often found at the entrances to the world of the dead, the hellhound will add to a spooktacular time in a graveyard or just at death's door, which could be anywhere!",
+        scores: [
+            4,
+            3,
+            5,
+            1,
+            1,
+            2,
+            1,
+            1,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Chupacabra",
+        photo: "https://img00.deviantart.net/879b/i/2017/054/1/4/chupacabra_by_000fesbra000-db00vay.jpg",
+        // IMAGE CREDIT: 000Fesbra000 on DeviantArt: https://www.deviantart.com/art/Chupacabra-665168506
+        description: 'With a name that can be literally translated as "goat-sucker", this reptilian creature really gets the job done. With a history of sucking blood from so many animals that his work was attributed to a satanic cult, you know the Chupacabra means business. If you have any friends who live and/or work on farms, this cryptid is surely the way to go!',
+        scores: [
+            1,
+            5,
+            2,
+            4,
+            2,
+            2,
+            4,
+            3,
+            4,
+            2
+        ]
+    },
+    {
+        name: "Jackalope",
+        photo: "http://cdn.shopify.com/s/files/1/0288/5332/files/jackalope_in_the_wild_large.jpg?11502701639502724114",
+        description: "This little guy has been taxidermied so many times, it's no wonder he's out for revenge. With an insatiable thirst for Whiskey and the ability to imitate any human voice, the Jackalope is sure to spook anyone who is willing to risk a trek into the Wyoming wilderness. This cryptid may be small, but watch out if you don't have stovepipes on your legs for protection, you'll be gored before you know it.",
+        scores: [
+            2,
+            2,
+            1,
+            2,
+            1,
+            4,
+            5,
+            5,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Jersey Devil",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/1/13/Jersey_Devil_Philadelphia_Post_1909.jpg",
+        description: "As the 13th child of a mother who cursed him, the Jersey Devil knows only how to express himself with blood-curdling screams. If you're looking for a cryptid who doesn't beat around the bush, look no further than this bat-winged, goat-footed monstrosity. With possible ties to witches and the Devil, you better be certain this is what you're looking for because you might just get spooked yourself!",
+        scores: [
+            5,
+            5,
+            5,
+            3,
+            1,
+            3,
+            2,
+            1,
+            2,
+            4
+        ]
+    },
+    {
+        name: "The Kraken",
+        photo: "http://2.bp.blogspot.com/-iF4qaYrRbaE/TpTWDRVirbI/AAAAAAAAHio/N6-HnX_bkos/s1600/kraken.gif",
+        description: "Found mainly around Scandinavia, The Kraken is fond of taking down entire vessels with his long, undulating tentacles. This squid-like creature is so large he is sometimes mistaken for an island, but don't be fooled! The Kracken will stop at nothing to take your friends down, so if a full-on ship sinking is your jam, this cryptid will surely do it justice!",
+        scores: [
+            1,
+            2,
+            1,
+            5,
+            5,
+            1,
+            3,
+            3,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Mothman",
+        photo: "https://imgix.ranker.com/user_node_img/50064/1001273421/original/the-mothman-saved-the-lives-of-miners-in-freiburg-germany-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces",
+        description: "Thanks to his moth wings, the Mothman can fly his way into any spooky time. With the possibilty of being an alian, the Mothman can swoop in with his glowing red eyes and scare the pants off of any friend you like. If you've got any friends with a penchant for UFO sightings or aliens in general, this cryptid will certainly deliver.",
+        scores: [
+            1,
+            1,
+            5,
+            2,
+            1,
+            4,
+            1,
+            2,
+            5,
+            5
+        ]
+    },
+    {
+        name: "The Cornish Owlman",
+        photo: "http://www.lazerhorse.org/wp-content/uploads/2014/07/Owlman-Mawnan-Artists-Impression.jpg",
+        description: "It's a bird! It's a plane! No, it's Owlman! Found in the village of Mawnan in England, this man-sized bird creature hoots his way into any vacation. This particular cryptid is not one for direct action, so if you're looking for just a mild spooking, the Cornish Owlman is where it's at.",
+        scores: [
+            4,
+            3,
+            4,
+            3,
+            2,
+            3,
+            2,
+            5,
+            1,
+            5
+        ]
+    },
+    {
+        name: "Snallygaster",
+        photo: "http://2.bp.blogspot.com/-oCl-18azx24/VNpT8Dse45I/AAAAAAAAUko/Qzc4PGWV2S8/s1600/Snallygaster%2B002.jpg",
+        description: "Although the name sounds silly, the Snallygaster is absolutely nothing to laugh at. This cryptid boasts a terrifying appearance of bird, reptile, and demon-like features. If you're looking for the unexpected, the Snallygaster is more than happy to swoop in and silently carry off his victims. He often likes to drink the blood of those he's captured, but whether he does or not is up to you!",
+        scores: [
+            1,
+            4,
+            3,
+            1,
+            1,
+            4,
+            1,
+            4,
+            2,
+            5
+        ]
+    },
+    {
+        name: "Yeti",
+        photo: "https://snowbrains.com/wp-content/uploads/2015/10/Yeti-590x330.jpg",
+        description: "Found mainly in Nepal, the Yeti loves nothing more than a good snowball fight. That or freaking out some of your closest friends. Having lived his entire life in the snowy mountains, the Yeti prefers to work alone. Marching around with stone weaponry and emerging out of nowhere through blizzards, the Yeti is a great option for some snow day fun.",
+        scores: [
+            2,
+            2,
+            1,
+            5,
+            1,
+            5,
+            2,
+            4,
+            1,
+            2
+        ]
     }
 ];
 
